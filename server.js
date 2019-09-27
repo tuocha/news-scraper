8d7ab27c8fe462fc9d350938e5b722f0debd5912
tuocha <kippon47@gmail.com>
2019-09-27 04:28:54 -0400

    fixed mongo connection to appease heroku

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 //mongodb connection
-mongoose.connect("mongodb://localhost/recipeScraper", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
